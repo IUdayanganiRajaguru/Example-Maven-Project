1c371afd69aec9b306d8fcda9da012c481d729ca
IUdayanganiRajaguru <iudayanganirajaguru@gmail.com>
2021-07-03 23:59:03 +0530

    Edit Ishara.java class

--- src/main/java/Ishara.java
+++ src/main/java/Ishara.java
@@ -2,5 +2,6 @@ public class Ishara {
     public static void main(String args[]){
         System.out.println("My Name Ishara");
         //System.out.println("My Name Ishara");
+        System.out.println("My second name is Udayangani");
     }
 }
